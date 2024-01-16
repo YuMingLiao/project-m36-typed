fd6d72abd28a54ca0b22fe5b9d8ebf49dd2adb68
Yu-Ming Liao <dycan0716@gmail.com>
2024-01-16 12:46:45 +0800

    project-m36-typed assume update to its own branch only. But once master is updated by some other connection, the connection becomes a not-head, thus headName cannot be found in headNameSTM. I could 1. open named branch for each connections and auto merge to master    pros: no UnknownHeadError    cons: why new branch needs automergetohead? it should use merge. 2. parametrise headName to support.    pros: I believe automergetohead is used in this situation (connections on same branch).    cons: may mess up project-m36-typed api logic

---
+++ package.nix
@@ -0,0 +1,71 @@
+let
+  snack-build-project-m36 = false; 
+project-m36 =
+    import /root/project-m36-on-last-nixos/snack-for-project-m36/lib.nix;
+in {
+  src = ./src;
+  name = "project-m36-typed";
+  packages = if snack-build-project-m36 then [project-m36] else [];
+  dependencies = [
+    "rio"
+    "base"
+    "generics-sop"
+    "basic-sop"
+    "QuickCheck"
+    "binary"
+    "binary-orphans"
+    "type-level-sets"
+    "random"
+    "tf-random"
+    "mtl"
+    "uuid"
+    "word8"
+    "quickcheck-instances"
+    "splitmix"
+    "binary-instances"
+  ] ++ (if snack-build-project-m36 then [] else ["project-m36"]);
+  ghcOpts = [
+    "-XHaskell2010"
+    "-XBangPatterns"
+    "-XBinaryLiterals"
+    "-XConstraintKinds"
+    "-XDataKinds"
+    "-XDefaultSignatures"
+    "-XDeriveDataTypeable"
+    "-XDeriveFoldable"
+    "-XDeriveFunctor"
+    "-XDeriveGeneric"
+    "-XDeriveTraversable"
+    "-XDerivingStrategies"
+    "-XDoAndIfThenElse"
+    "-XEmptyDataDecls"
+    "-XExistentialQuantification"
+    "-XFlexibleContexts"
+    "-XFlexibleInstances"
+    "-XFunctionalDependencies"
+    "-XGADTs"
+    "-XGeneralizedNewtypeDeriving"
+    "-XInstanceSigs"
+    "-XKindSignatures"
+    "-XLambdaCase"
+    "-XMultiParamTypeClasses"
+    "-XMultiWayIf"
+    "-XNamedFieldPuns"
+    "-XNoImplicitPrelude"
+    "-XOverloadedStrings"
+    "-XPartialTypeSignatures"
+    "-XPatternGuards"
+    "-XPolyKinds"
+    "-XRankNTypes"
+    "-XRecordWildCards"
+    "-XScopedTypeVariables"
+    "-XStandaloneDeriving"
+    "-XTupleSections"
+    "-XTypeApplications"
+    "-XTypeFamilies"
+    "-XTypeOperators"
+    "-XTypeSynonymInstances"
+    "-XViewPatterns"
+  ];
+  extensions = [ ];
+}
